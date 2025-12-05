848611aba27d3ead497c924fe279735de66da764
Kayv1e <krutojpacanivan@gmail.com>
2025-12-06 00:01:15 +0300

    Added HorizontalLine and Rectangle structures

--- main.cpp
+++ main.cpp
@@ -32,10 +32,38 @@ namespace topit
     p_t d;
   };
 
+  struct HorizontalLine: IDraw
+  {
+    HorizontalLine(p_t s, p_t e);
+    p_t begin() const override;
+    p_t next(p_t) const override;
+    p_t start, end;
+  };
+
+  struct Rectangle: IDraw
+  {
+    Rectangle(p_t s, p_t e);
+    p_t begin() const override;
+    p_t next(p_t) const override;
+    p_t start, end;
+  };
+
+  // HOME TASK
+  // add 2-3 more shapes (e.g. vertical line, horizontal line, diagonal line 45 degrees, or my own)
+
+  // extend with points from a shape
   size_t points(const IDraw & d, p_t ** pts, size_t & s);
+
+  // min and max on x and y from all points and create frame
   f_t frame(const p_t * pts, size_t s);
+
+  // get rows and columns from frame
   char * canvas(f_t fr, char filler);
+
+  // convert dot coordinates to 2d array coordinates
   void paint(char * cnv, f_t fr, p_t p, char filler);
+
+  // display 2d array using rows and columns from frame
   void flush(std::ostream & os, const char * cnv, f_t fr);
 }
 
@@ -97,6 +125,55 @@ topit::p_t topit::Dot::next(p_t prev) const
     throw std::logic_error("Bad implementation");
   }
   return d;
+}
+
+topit::Rectangle::Rectangle(p_t s, p_t e):
+  start(s),
+  end(e)
+{}
+
+topit::p_t topit::Rectangle::begin() const
+{
+  return start;
+}
+
+topit::p_t topit::Rectangle::next(p_t prev) const
+{
+  if (prev.y == start.y && start.x <= prev.x && prev.x < end.x) {
+    return {prev.x + 1, prev.y};
+  }
+  if (prev.x == end.x && start.y >= prev.y && prev.y > end.y) {
+    return {prev.x, prev.y - 1};
+  }
+  if (prev.y == end.y && start.x < prev.x && prev.x <= end.x) {
+    return {prev.x - 1, prev.y};
+  }
+  if (prev.x == start.x && start.y > prev.y && prev.y >= end.y) {
+    return {prev.x, prev.y + 1};
+  }
+  throw std::logic_error("Bad implementation");
+}
+
+topit::HorizontalLine::HorizontalLine(p_t s, p_t e):
+  IDraw(),
+  start(s),
+  end(e)
+{}
+
+topit::p_t topit::HorizontalLine::begin() const
+{
+  return start;
+}
+
+topit::p_t topit::HorizontalLine::next(p_t prev) const
+{
+  if (prev == end) {
+    return start;
+  }
+  if (prev.y == start.y && start.x <= prev.x && prev.x < end.x) {
+    return {prev.x + 1, prev.y};
+  }
+  throw std::logic_error("Bad implementation");
 }
 
 bool topit::operator==(p_t a, p_t b)
